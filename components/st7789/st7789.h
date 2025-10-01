8f3b9f2baf9b789200dde42a98220c1d667b8adb
Adrian <adrianpuscasu777@gmail.com>
2025-10-01 15:06:51 +0300

    Improving display driver and updating tests

--- components/st7789/st7789.h
+++ components/st7789/st7789.h
@@ -36,4 +36,66 @@ esp_err_t st7789_init(void);
  */
 void st7789_test(void);
 
+/**
+ * @brief Draw a single pixel at specified coordinates
+ * 
+ * @param x X coordinate (0-239)
+ * @param y Y coordinate (0-239) 
+ * @param color 16-bit RGB565 color value
+ */
+void st7789_draw_pixel(uint16_t x, uint16_t y, uint16_t color);
+
+/**
+ * @brief Fill a rectangular area with specified color
+ * 
+ * @param x X coordinate of top-left corner
+ * @param y Y coordinate of top-left corner
+ * @param w Width of rectangle
+ * @param h Height of rectangle
+ * @param color 16-bit RGB565 color value
+ */
+void st7789_fill_rect(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint16_t color);
+
+/**
+ * @brief Draw a single character at specified position
+ * 
+ * @param x X coordinate for character placement
+ * @param y Y coordinate for character placement  
+ * @param c Character to draw (printable ASCII 32-126)
+ * @param color 16-bit RGB565 foreground color
+ * @param bg_color 16-bit RGB565 background color
+ */
+void st7789_draw_char(uint16_t x, uint16_t y, char c, uint16_t color, uint16_t bg_color);
+
+/**
+ * @brief Draw a text string at specified position
+ * 
+ * Supports newline (\n) and carriage return (\r) characters.
+ * Automatically wraps text to next line if it exceeds display width.
+ * 
+ * @param x X coordinate for text start position
+ * @param y Y coordinate for text start position
+ * @param str Null-terminated string to draw
+ * @param color 16-bit RGB565 foreground color
+ * @param bg_color 16-bit RGB565 background color
+ */
+void st7789_draw_string(uint16_t x, uint16_t y, const char* str, uint16_t color, uint16_t bg_color);
+
+/**
+ * @brief Clear entire screen with specified color
+ * 
+ * @param color 16-bit RGB565 color value
+ */
+void st7789_clear_screen(uint16_t color);
+
+// Common RGB565 color definitions for convenience
+#define ST7789_BLACK   0x0000  // Black
+#define ST7789_WHITE   0xFFFF  // White  
+#define ST7789_RED     0xF800  // Red
+#define ST7789_GREEN   0x07E0  // Green
+#define ST7789_BLUE    0x001F  // Blue
+#define ST7789_YELLOW  0xFFE0  // Yellow
+#define ST7789_CYAN    0x07FF  // Cyan
+#define ST7789_MAGENTA 0xF81F  // Magenta
+
 #endif // ST7789_H
